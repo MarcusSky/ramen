8f2e6541bbe0d97381064c54784f7e0beb531660
Marcus Vieira <marcusbrunovieira@gmail.com>
2018-06-25 14:54:02 -0300

    Create initial structure to list Pull Requests

--- lib/ramen.ex
+++ lib/ramen.ex
@@ -1,18 +1,32 @@
+defmodule PullRequest do
+  defstruct [:title]
+end
+
+defmodule Decoder do
+  @spec decode(%{}) :: %PullRequest{}
+  def decode(payload) do
+    %{"title" => title} = payload
+
+    %PullRequest{title: title}
+  end
+end
+
 defmodule Ramen do
-  @moduledoc """
-  Documentation for Ramen.
-  """
+  @spec new(String.t()) :: %{access_token: String.t()}
+  def new(access_token) do
+    %{access_token: access_token}
+  end
 
-  @doc """
-  Hello world.
+  @spec list_pull_requests(String.t(), String.t(), struct()) ::
+          {:ok, [%PullRequest{}]} | {:error, String.t()}
+  def list_pull_requests(owner, repository, client) do
+    case do_list_pull_requests(owner, repository, client) do
+      {200, pull_requests, _} -> {:ok, Enum.map(pull_requests, &Decoder.decode(&1))}
+      {_, error_body, _} -> {:error, error_body}
+    end
+  end
 
-  ## Examples
-
-      iex> Ramen.hello
-      :world
-
-  """
-  def hello do
-    :world
+  defp do_list_pull_requests(owner, repository, client) do
+    Tentacat.Pulls.filter(Tentacat.Client.new(client), owner, repository, %{state: "open"})
   end
 end
