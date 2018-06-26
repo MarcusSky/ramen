f853b8f6fc5ba2203a2f0ab744a7bd0aeb46be3a
Marcus Vieira <marcusbrunovieira@gmail.com>
2018-06-25 23:32:11 -0300

    Remove `tentacat`

--- mix.exs
+++ mix.exs
@@ -14,14 +14,13 @@ defmodule Ramen.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger, :tentacat]
+      extra_applications: [:logger]
     ]
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:tentacat, "~> 1.0"},
       {:poison, "~> 3.1"},
       {:httpoison, "~> 1.0", override: true}
       # {:dep_from_hexpm, "~> 0.3.0"},
