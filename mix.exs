5dab9083c6b7710c5d3c09266bed7a91686eb777
Marcus Vieira <marcusbrunovieira@gmail.com>
2018-07-03 18:42:42 -0300

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Ramen.MixProject do
   def project do
     [
       app: :ramen,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       description: description(),
