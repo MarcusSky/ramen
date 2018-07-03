80826a70ea4a0b7a3fc0cab4fde34d0bada3d6d2
Marcus Vieira <marcusbrunovieira@gmail.com>
2018-07-03 10:43:43 -0300

    Add `package` to `mix.exs`

--- mix.exs
+++ mix.exs
@@ -7,6 +7,8 @@ defmodule Ramen.MixProject do
       version: "0.1.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
+      description: description(),
+      package: package(),
       deps: deps()
     ]
   end
@@ -18,10 +20,6 @@ defmodule Ramen.MixProject do
     ]
   end
 
-  defp description do
-    "A Richer GitHub API Client."
-  end
-
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
@@ -29,6 +27,18 @@ defmodule Ramen.MixProject do
       {:httpoison, "~> 1.0"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+    ]
+  end
+
+  defp description() do
+    "A Richer GitHub API Client."
+  end
+
+  defp package() do
+    [
+      name: "ramen",
+      licenses: ["Apache 2.0"],
+      links: %{"GitHub" => "https://github.com/MarcusSky/ramen"}
     ]
   end
 end
